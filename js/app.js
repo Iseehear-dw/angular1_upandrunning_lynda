9e0f5be0b66d87fecba19a09e825872d165cc6a7
dtw <dtw2005@gmail.com>
2016-09-16 15:10:34 -0400

    add ngAnimate

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function (angular) {
+(function () {
 
     "use strict";
 
@@ -8,11 +8,18 @@
     ]);
 
     myApp.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/list', {
-            templateUrl: 'partials/list.html',
-            controller: 'ListController'
-        }).otherwise({
-            redirectTo: '/list'
-        });
+        $routeProvider
+            .when('/list', {
+                templateUrl: 'partials/list.html',
+                controller: 'ListController'
+            })
+            .when('/details/:itemId', {
+                templateUrl: 'partials/details.html',
+                controller: 'DetailsController'
+            })
+            .otherwise({
+                redirectTo: '/list'
+            });
     }]);
-})(window.angular);
+
+})();
